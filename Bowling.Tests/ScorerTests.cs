babec37d9fff5810667974aaa14f7fb333b27dc2
pavlosmcg <pavlosmcg@users.noreply.github.com>
2017-08-01 18:44:39 +0100

    Score up a game with some non-zero scores

--- Bowling.Tests/ScorerTests.cs
+++ Bowling.Tests/ScorerTests.cs
@@ -9,22 +9,44 @@ namespace Bowling.Tests
         public void Calculate_ReturnsZero_WhenAllFramesAreMisses()
         {
             var unit = new Scorer();
-            var input = new string[,] {
-                {"-", "-"},
-                {"-", "-"},
-                {"-", "-"},
-                {"-", "-"},
-                {"-", "-"},
-                {"-", "-"},
-                {"-", "-"},
-                {"-", "-"},
-                {"-", "-"},
-                {"-", "-"},
+            var input = new string[] {
+                "-", "-",
+                "-", "-",
+                "-", "-",
+                "-", "-",
+                "-", "-",
+                "-", "-",
+                "-", "-",
+                "-", "-",
+                "-", "-",
+                "-", "-",
             };
 
             int score = unit.Calculate(input);
 
             Assert.Equal(0, score);
+        }
+
+        [Fact]
+        public void Calculate_Returns50_WhenAllFramesAddUpTo5()
+        {
+            var unit = new Scorer();
+            var input = new string[]{
+                "5", "-",
+                "5", "-",
+                "5", "-",
+                "2", "3",
+                "3", "2",
+                "4", "1",
+                "-", "5",
+                "-", "5",
+                "-", "5",
+                "-", "5",
+            };
+
+            int score = unit.Calculate(input);
+
+            Assert.Equal(50, score);
         }
     }
 }
--- Bowling/Scorer.cs
+++ Bowling/Scorer.cs
@@ -4,8 +4,17 @@ namespace Bowling
 {
     public class Scorer
     {
-        public int Calculate(string[,] frames) {
-            return 0;
+        public int Calculate(string[] turns) {
+            int total = 0;
+
+            for (int i = 0; i < turns.Length; i++)
+            {
+                int turnScore;
+                if (int.TryParse(turns[i], out turnScore))
+                    total += turnScore;
+            }
+
+            return total;
         }
     }
 }
